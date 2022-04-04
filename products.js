b1f00ddb0ad2d95af97beb04f52a0e2d4e05224d
Alex <alexgmiller00@gmail.com>
2022-04-04 11:45:40 -0700

    Adds get,post,patch,delete requests

---
+++ products.js
@@ -0,0 +1,35 @@
+/* global Vue, axios */
+
+var App = {
+  data: function () {
+    return {
+      message: "Hello from JavaScript!",
+    };
+  },
+  methods: {
+    created: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        console.log(response.data);
+      });
+    },
+    createProduct: function () {
+      var params = { name: "", description: "", price: 1, image_url: "" };
+      axios.post("http://localhost:3000/products", params).then((response) => {
+        console.log(response.data);
+      });
+    },
+    updateProduct: function () {
+      var params = { name: "", description: "", price: 1, image_url: "" };
+      axios.patch("http://localhost:3000/products/:id", params).then((response) => {
+        console.log(response.data);
+      });
+    }
+    deleteProduct: function () {
+      axios.delete("http://localhost:3000/products/:id").then(response => {
+        console.log(response);
+      });
+    },
+  },
+};
+
+Vue.createApp(App).mount("#app");
